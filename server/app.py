e3b2704b26d9414a970307d641ee340738588daf
Karina Mogha <moghakarina@gmail.com>
2024-12-12 16:29:35 -0500

    done

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
@@ -46,7 +45,19 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self,id):
+        plant =Plant.query.filter_by(id =id).first()
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+    def delete(self, id):
+        plant = Plant.query.filter_by(id =id).first()
+        db.session.delete(plant)
+        db.session.commit()
 
+        return make_response(jsonify({}),204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
